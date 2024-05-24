bddaa431ed4404adeb40a2a21e103f6c5564fd21
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-05-24 18:24:48 +0700

    chore(unguided): add unguided first task

--- graph-dan-tree/unguided/tugas1.cpp
+++ graph-dan-tree/unguided/tugas1.cpp
@@ -1,73 +1,60 @@
-#include <iostream>
 #include <iomanip>
-#include <string>
+#include <iostream>
 
 using namespace std;
 
-void selection_sort(char word_081[], int length_081) {
-  int temp, min, i, j;
+int main() {
+  int k_081, v_081, n_081 = 0, w_081 = 8;
 
-  for (i = 0; i < length_081; i++) {
-    min = i;
+  cout << "Menghitung Jarak Tempuh" << endl;
+  cout << "=======================" << endl;
+  cout << "Masukkan jumlah data: ";
+  cin >> n_081;
 
-    for (j = i + 1; j < length_081; j++) {
-      if (word_081[j] > word_081[min]) continue;
+  cout << "=======================" << endl;
+  cout << "Silahkan masukkan nama simpul" << endl;
 
-      min = j;
+  string simpul_081[n_081];
+
+  for (k_081 = 0; k_081 < n_081; k_081++) {
+    cout << "Simpul ke-" << k_081 + 1 << " : ";
+    cin >> simpul_081[k_081];
+  }
+
+  cout << "=======================" << endl;
+  cout << "Silahkan masukkan bobot antar simpul" << endl;
+
+  int bobot_081[n_081][n_081];
+
+  for (k_081 = 0; k_081 < n_081; k_081++) {
+    for (v_081 = 0; v_081 < n_081; v_081++) {
+      cout << "Bobot dari " << simpul_081[k_081] << " ke " << simpul_081[v_081] << " : ";
+      cin >> bobot_081[k_081][v_081];
+    }
+  }
+
+  cout << "=======================" << endl;
+  cout << "Matriks bobot antar simpul" << endl;
+
+  for (k_081 = 0; k_081 <= n_081; k_081++) {
+    if (k_081 == 0) {
+      cout << setw(w_081) << " ";
+      continue;
     }
 
-    temp = word_081[i];
-    word_081[i] = word_081[min];
-    word_081[min] = temp;
-  }
-}
-
-void binary_search(char word_081[], int length_081, char target_081) {
-  int start = 0, end = length_081 - 1, mid, is_found = false;
-
-  while (!is_found && start <= end) {
-    mid = (start + end) / 2;
-
-    if (word_081[mid] == target_081) is_found = true;
-    else if (word_081[mid] < target_081) start = mid + 1;
-    else end = mid - 1;
+    cout << setw(w_081) << simpul_081[k_081 - 1];
   }
 
-  if (is_found) cout << "Data ditemukan pada indeks ke-" << mid << endl;
-  else cout << "Data tidak ditemukan" << endl;
-}
-
-int main() {
-  string tempWord_081;
-
-  cout << "Mencari huruf dalam kata" << endl;
-  cout << "========================" << endl;
-  cout << "Masukkan kata : ";
-  getline(cin, tempWord_081);
-
-  int length_081 = tempWord_081.length();
-  char word_081[length_081 + 1], target_081;
-
-  for (int i = 0; i < length_081; i++) {
-    if (tempWord_081[i] == ' ' || tempWord_081[i] == '\0') continue;
-
-    word_081[i] = tolower(tempWord_081[i]);
-  }
-
-  cout << "Masukkan huruf yang dicari : ";
-  cin >> target_081;
   cout << endl;
 
-  cout << "Data diurutkan : ";
-  selection_sort(word_081, length_081);
+  for (k_081 = 0; k_081 < n_081; k_081++) {
+    cout << setw(w_081) << simpul_081[k_081];
 
-  for (int i = 0; i < length_081; i++) {
-    cout << setw(3) << word_081[i];
+    for (v_081 = 0; v_081 < n_081; v_081++) {
+      cout << setw(w_081) << bobot_081[k_081][v_081];
+    }
+    cout << endl;
   }
-
-  cout << "\n" << endl;
-
-  binary_search(word_081, length_081, tolower(target_081));
 
   return 0;
 }
